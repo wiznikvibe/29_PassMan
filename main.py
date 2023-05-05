7b6ed193b35576b747d7da98d41c45a9b9c506b0
wiznikvibe <nikhilshetty439@gmail.com>
2023-05-05 19:21:34 +0530

    Complete Password Manager

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def gen_pass():
     pwdList = pwd_let + pwd_num + pwd_sym
     random.shuffle(pwdList)
     password = "".join(pwdList)
-    pwd_entry.insert(0,password)
+    password_entry.insert(0,password)
    
 
 
@@ -53,47 +53,83 @@ def pass_save():
 
     if len(website)==0 or len(password)==0:
         messagebox.showinfo(title='OOps', message="Please make sure to not leave any fields empty.")
-        
+    
     else:
-        with open('data.json','w') as data_file:
-            json.dump(new_data, data_file)
-            web_entry.delete(0,END)
-            pwd_entry.delete(0,END) 
+        try:
+            # If the file already exists then read the file
+            with open('data.json','r') as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            # If file not found create the file and dump the new data into it
+            with open('data.json','w') as data_file:
+                json.dump(new_data,data_file, indent=4)
+        else:
+            # if Try block executes then update the existing data 
+            data.update(new_data)
+
+            with open('data.json','w') as data_file:
+            
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0,END)
+            password_entry.delete(0,END) 
+
+# ---------------------------- Search Credentials --------------------- #
+
+def search_cred():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")       
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Passcode Manager")
-window.config(padx=20,pady=20)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(height=200,width=200)
+canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100,100,image=logo_img)
-canvas.grid(row=0,column=1)
+canvas.grid(row=0, column=1)
 
-website = Label(text='Website:')
-website.grid(column=0,row=1)
-web_entry = Entry(width=35)
-web_entry.focus()
-web_entry.grid(column=1,row=1, columnspan=2)
+#Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
 
-user = Label(text='Username/Email:')
-user.grid(column=0,row=2)
-user_entry = Entry(width=35)
-user_entry.insert(END, 'nikhilshetty00@gmail.com')
-user_entry.grid(column=1,row=2, columnspan=2)
+#Entries
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
+website_entry.focus()
 
-pwd = Label(text='Password:')
-pwd.grid(column=0,row=3)
-pwd_entry = Entry(width=21)
-pwd_entry.grid(column=1,row=3,columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "example@xyz.com")
 
-pwd_gen = Button(text="Generate Password",command=gen_pass)
-pwd_gen.grid(column=3,row=3)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
-add_but = Button(text='Add', width=36, command=pass_save)
-add_but.grid(column=1,row=4,columnspan=2)
+# Buttons
+search_button = Button(text="Search", width=13, command=search_cred)
+search_button.grid(row=1, column=2)
 
+generate_password_button = Button(text="Generate Password", command=gen_pass)
+generate_password_button.grid(row=3, column=2)
 
+add_button = Button(text="Add", width=36, command=pass_save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
